ec4b78a2d35d304ed041737bf44e9d15933030ae
anzul <anzulic@gmail.com>
2025-11-22 14:15:47 +0500

    Сравнение треугольников: метод equals + тесты

--- sandbox/src/main/java/ru/stqa/reometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/reometry/figures/Triangle.java
@@ -1,5 +1,7 @@
 package ru.stqa.reometry.figures;
 
+import java.util.Objects;
+
 public record Triangle(double a, double b, double c) {
 
     //Конструктор
@@ -10,6 +12,20 @@ public record Triangle(double a, double b, double c) {
         if ((a+b) < c || (b+c) < a || (c+a) < b){
             throw new IllegalArgumentException ("Triangle side should be non-negative");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Triangle triangle = (Triangle) o;
+        return (Double.compare(this.a, triangle.a) == 0 && Double.compare(this.b, triangle.b) == 0 && Double.compare(this.c, triangle.c) == 0)
+                || (Double.compare(this.a, triangle.b) == 0 && Double.compare(this.b, triangle.c) == 0 && Double.compare(this.c, triangle.a) == 0)
+                || (Double.compare(this.a, triangle.c) == 0 && Double.compare(this.b, triangle.a) == 0 && Double.compare(this.c, triangle.b) == 0);
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
     }
 
     //Метод для периметра
